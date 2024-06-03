0a857f0f3173b7b9b672765f7d1b4ecd2bf86a59
Masmat97 <matteo.mascio@libero.it>
2024-06-03 17:46:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+  components: {
+    AppMain,
+    AppHeader,
+    AppFooter
+  },
+  props: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  },
+}
+
+</script>
+
+<template>
+  <AppHeader /> 
+  <AppMain />
+  <AppFooter />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+
+        };
+        computed: {
+
+        };
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<template>
+    <div class="hello">
+        <h5>"FORGET THE TRENDY PIZZA SHOPS, THIS HIDDEN SPOT MAKES THE BEST NEW YORK-STYLE PIZZA SLICE IN NAPLES"</h5>
+        <p>WASHINGTON POST 2018</p>
+    </div>
+</template>
+
+<style scoped>
+.hello {
+    background-image: url(/public/img/h3-testimonials-bckgrnd.jpg);
+    /* height: 30rem; */
+}
+
+h5 {
+padding-top: 15%;
+text-align: center;
+padding-left: 15%;
+padding-right: 15%;
+
+}
+
+p {
+    font-size: 10px;
+    text-align: center;
+    color:#EE2852 ;
+    padding-bottom: 10rem;
+}
+</style>
